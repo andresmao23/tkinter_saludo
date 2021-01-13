9d5fbd811b08ebd1e2d0263c735d56729909b24b
Andres Caicedo <amcaicedo@unimayor.edu.co>
2021-01-13 11:03:39 -0500

    Se agrega el titilo de la ventana y se establece el ancho y alto maximos

--- main.py
+++ main.py
@@ -3,5 +3,12 @@ from vista.aplicacion import Aplicacion
 
 if __name__ == "__main__":
     root = tk.Tk()
+    # Crea la aplicación
     app = Aplicacion(ventana=root)
+    #
+    # here are method calls to the window manager class
+    #
+    app.ventana.title("Proyecto para saludar a una persona")
+    app.ventana.maxsize(1000, 400)
+    # start the program
     app.mainloop()
